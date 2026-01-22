74690943f810755420111bff505e042432b57040
kseniya <kiselevakseniya45@gmail.com>
2026-01-22 11:42:23 +0300

    add digit dictionary implementation

---
+++ src/main/java/dictionary/impl/DigitDictionary.java
@@ -0,0 +1,24 @@
+package dictionary.impl;
+import dictionary.service.DictionaryService;
+
+import java.util.Map;
+
+import java.util.HashMap;
+public class DigitDictionary implements DictionaryService {
+    Map<String, String> data = new HashMap<>();
+    @Override
+    public void add(String key, String value) {
+        data.put(key, value);
+    }
+
+    @Override
+    public String find(String key) {
+        return data.get(key);
+    }
+
+    @Override
+    public void remove(String key) {
+        data.remove(key);
+    }
+}
+
