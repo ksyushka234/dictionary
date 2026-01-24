47dfd8600a0a4b7f319627ce722e51d232440031
kseniya <kiselevakseniya45@gmail.com>
2026-01-24 19:09:56 +0300

    Добавлена загрузка словарей из файла

--- src/main/java/dictionary/DictionaryTest.java
+++ src/main/java/dictionary/DictionaryTest.java
@@ -1,5 +1,6 @@
 package dictionary;
 
+import dictionary.impl.DigitDictionary;
 import dictionary.impl.LatinDictionary;
 import dictionary.io.DictionaryFileLoader;
 import dictionary.service.DictionaryService;
@@ -10,5 +11,9 @@ import dictionary.service.DictionaryService;
             DictionaryFileLoader.loadFromFile(d1, "latin.txt");
             System.out.println(d1.find("abcd"));
             System.out.println(d1.find("test"));
+            DictionaryService d2 = new DigitDictionary();
+            DictionaryFileLoader.loadFromFile(d2, "digit.txt");
+            System.out.println(d2.find("12345"));
+            System.out.println(d2.find("00001"));
         }
     }
