752f410ae8af4da196381a45680bbc6b2f2c3a7a
kseniya <kiselevakseniya45@gmail.com>
2026-01-24 18:46:57 +0300

    Проверка загрузки словаря из файла

--- src/main/java/dictionary/DictionaryTest.java
+++ src/main/java/dictionary/DictionaryTest.java
@@ -4,13 +4,10 @@ import dictionary.impl.LatinDictionary;
 import dictionary.io.DictionaryFileLoader;
 import dictionary.service.DictionaryService;
 
-public class DictionaryTest {
-    public static void main(String args[]){
-        DictionaryService d1 = new LatinDictionary();
-        System.out.println(System.getProperty("user.dir"));
-        DictionaryFileLoader.loadFromFile(d1, "latin.txt");
-                d1.add("lowd","hello");
-                d1.find("lowd");
-                d1.remove("lowd");
+    public class DictionaryTest {
+        public static void main(String args[]){
+            DictionaryService d1 = new LatinDictionary();
+            System.out.println(System.getProperty("user.dir"));
+            DictionaryFileLoader.loadFromFile(d1, "latin.txt");
+        }
     }
-}
