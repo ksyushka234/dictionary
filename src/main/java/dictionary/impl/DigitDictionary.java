66ad003e17257e8cb90f0b6f3f8680b232441b1a
kseniya <kiselevakseniya45@gmail.com>
2026-01-24 18:16:47 +0300

    правка длины ключа

--- src/main/java/dictionary/impl/DigitDictionary.java
+++ src/main/java/dictionary/impl/DigitDictionary.java
@@ -8,7 +8,7 @@ public class DigitDictionary implements DictionaryService {
     Map<String, String> data = new HashMap<>();
     @Override
     public void add(String key, String value) {
-        if (key.length()==4){
+        if (key.length()==5){
             boolean valid = true;
             for (int i = 0;i< key.length();i++){
                 char c = key.charAt(i);
