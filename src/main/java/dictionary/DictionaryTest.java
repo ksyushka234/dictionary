eac371dc07f1a17cb9f932b7803d7735a4f93855
kseniya <kiselevakseniya45@gmail.com>
2026-02-09 21:10:47 +0300

    Добавили возможность проверять успешность добавления

--- src/main/java/dictionary/DictionaryTest.java
+++ src/main/java/dictionary/DictionaryTest.java
@@ -7,12 +7,13 @@ import dictionary.service.DictionaryService;
 
     public class DictionaryTest {
         public static void main(String args[]){
+            DictionaryFileLoader fileLoader = new DictionaryFileLoader();
             DictionaryService d1 = new LatinDictionary();
-            DictionaryFileLoader.loadFromFile(d1, "latin.txt");
+            fileLoader.loadFromFile(d1, "latin.txt");
             System.out.println(d1.find("abcd"));
             System.out.println(d1.find("test"));
             DictionaryService d2 = new DigitDictionary();
-            DictionaryFileLoader.loadFromFile(d2, "digit.txt");
+            fileLoader.loadFromFile(d2, "digit.txt");
             System.out.println(d2.find("12345"));
             System.out.println(d2.find("00001"));
         }
