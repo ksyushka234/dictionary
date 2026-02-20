fcd1ba71446a64f9168b034f866dd52c11ea499d
kseniya <kiselevakseniya45@gmail.com>
2026-02-20 03:05:42 +0300

    Добавлены методы поиска в репозитории

--- src/main/java/dictionary/db/repository/EntryRepository.java
+++ src/main/java/dictionary/db/repository/EntryRepository.java
@@ -1,7 +1,13 @@
 package dictionary.db.repository;
 
+import dictionary.db.entity.DictionaryEntity;
 import dictionary.db.entity.EntryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EntryRepository extends JpaRepository<EntryEntity, Long> {
+    Optional<EntryEntity> findByDictionaryAndEntryKey(DictionaryEntity dictionary, String entryKey);
+    List<EntryEntity> findByEntryKeyContainingIgnoreCase(String entryKey);
 }
--- src/main/java/dictionary/db/repository/EntryValueRepository.java
+++ src/main/java/dictionary/db/repository/EntryValueRepository.java
@@ -3,5 +3,8 @@ package dictionary.db.repository;
 import dictionary.db.entity.EntryValueEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EntryValueRepository extends JpaRepository<EntryValueEntity, Long> {
+    List<EntryValueEntity> findByValueContainingIgnoreCase(String value);
 }
